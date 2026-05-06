cc5c53437db917ac1ec08a584789df41a664a462
polin-sv <polinasvetlay@icloud.com>
2026-05-06 17:30:59 +0300

    task02

--- Task02/logic.cpp
+++ Task02/logic.cpp
@@ -4,3 +4,38 @@
 // Дан вектор целочисленных значений. Необходимо разработать эффективный алгоритм
 // и реализовать функцию, которая вычисляет среднее арифметическое ненулевых 
 // элементов вектора.
+#include "logic.h"
+double count_elements(int* array, int size) {
+	int count = 0;
+	
+
+	for (int i = 0; i < size; i++)
+	{
+		if (array[i] != 0) {
+			count++;
+		}
+	}
+
+	return count;
+}
+
+double sum_of_elements(int* array, int size) {
+	int sum = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (array[i] != 0) {
+			sum += array[i];
+		}
+	}
+
+	return sum;
+}
+
+double calculate_arithmetical_mean_of_nonzero_elements(int* array, int size) {
+	if (size <= 0) {
+		return 0;
+	}
+
+	return sum_of_elements(array, size) / count_elements(array, size);
+}
