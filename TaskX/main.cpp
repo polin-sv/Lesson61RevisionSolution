6a0d4c6f61645f2c7c2e7a0343b700fc6e3b636f
polin-sv <polinasvetlay@icloud.com>
2026-05-06 18:36:02 +0300

    soft taskX

--- TaskX/main.cpp
+++ TaskX/main.cpp
@@ -11,13 +11,7 @@ int main() {
 
 	print("array: " + convert(array, size) + "\n");
 
-	// Обработка результатов экзамена
-// Введите исходные данные: 5 4 4 5 3 4 3 4 5 3 4 4 3 4 4 3 5 3 3 4 5 5 5 5 4 5 5 5 2 5
-// Результаты самостоятельной работы (или экзамена):
-//	пятерок 40 % (12)
-//	четверок 33.3 % (10)
-//	троек 23.3 % (7)
-//	двоек 3.4 % (1)
+
 
 	print("mark five " + to_string(result_of_marks(array, size, 5)) + "%\n");
 	print("mark four " + to_string(result_of_marks(array, size, 4)) + "%\n");
